cf69b7f839d949b4afff895cdd953a79210d02e9
Leonardo Vieira Spinosa <107893252+LeoSpinosa@users.noreply.github.com>
2023-10-05 20:09:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let container = document.querySelector('.container')
+let formulario = document.querySelector('.formulario')
+let pesquisa = document.querySelector('.form-control')
+
+async function getFilmes(buscar){
+    let request = await fetch(`http://www.omdbapi.com/?s=${buscar}&apikey=e6c43dc6`)
+    let data = await request.json()
+    
+    console.log(data);
+
+    data.Search.forEach((filmes) => {
+        container.innerHTML += `
+        <div class="card">
+            <p>Titulo: ${filmes['Title']} </p>
+            <img src="${filmes['Poster']}" alt="">
+        </div>`
+
+    });
+}
+
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+    container.innerHTML = ''
+    getFilmes(pesquisa.value)
+    pesquisa.value = '' 
+})
+
